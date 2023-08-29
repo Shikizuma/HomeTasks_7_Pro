3c036c0c9f7f7fc2a2bfe5fecd77f581347a71bd
Miroslav <myroslav.bashkirov@gmail.com>
2023-08-29 18:12:49 +0300

    Add project files.

---
+++ HomeTasks_7_Pro/Program.cs
@@ -0,0 +1,17 @@
+﻿using Task2;
+
+namespace HomeTasks_7_Pro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestObsolete testObsolete = new TestObsolete();
+            #if DEBUG
+                testObsolete.TestMethod();
+            #endif
+            testObsolete.TestMethod("Test");
+            testObsolete.TestMethod("Test", 22);
+        }
+    }
+}
---
+++ HomeTasks_7_Pro/TestObsolete.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class TestObsolete
+    {
+        [Obsolete("Цей метод застарілий, його не можна використовувати", true)]
+        public void TestMethod()
+        {
+            Console.WriteLine("Застарілий метод");
+        }
+
+        [Obsolete("Цей метод устарів.")]
+        public void TestMethod(string name)
+        {
+            Console.WriteLine($"Привіт, {name}");
+        }
+        public void TestMethod(string name, int age)
+        {
+            Console.WriteLine($"Привіт, {name}. Тобі {age} років");
+        }
+    }
+}
